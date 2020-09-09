0e50c665cfdfc0c08a6f5c327b840b5ca579537e
andric <rodomir3119841985@gmail.com>
2020-09-09 09:57:08 +0300

    victorina

--- app/js/main.js
+++ app/js/main.js
@@ -5,55 +5,43 @@ JQuery(document).ready(function($) {
   })
 })
 */
-
 // burger
 // при нажатие возникает меню с затеменнием в мобильной версии
-(()=> {
-
-let btn = document.querySelector('.menu-btn');
-let nav = document.getElementById('nav');
-
-btn.addEventListener('click',function () {
-  nav.classList.toggle('active');
-});
-
-
+(() => {
+  let btn = document.querySelector('.menu-btn');
+  let nav = document.getElementById('nav');
+  btn.addEventListener('click', function () {
+    nav.classList.toggle('active');
+  });
 })();
-
 // accordion
-(()=>{
+(() => {
   const acc = document.getElementsByClassName("accordion__header");
   let i;
-
   for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
+    acc[i].onclick = function () {
       this.classList.toggle("active");
       this.nextElementSibling.classList.toggle("show");
     }
   }
 })();
-
 // прокрутка по якорям
-(()=> {
+(() => {
   // // собираем все якоря; устанавливаем время анимации и количество кадров
 // const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
 //       animationTime = 300,
 //       framesCount = 20;
-
 // anchors.forEach(function(item) {
 //   // каждому якорю присваиваем обработчик события
 //   item.addEventListener('click', function(e) {
 //     // убираем стандартное поведение
 //     e.preventDefault();
-
 //     // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
 //     let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top;
-
 //     // запускаем интервал, в котором
 //     let scroller = setInterval(function() {
 //       // считаем на сколько скроллить за 1 такт
 //       let scrollBy = coordY / framesCount;
-
 //       // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
 //       // и дно страницы не достигнуто
 //       if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
@@ -68,9 +56,6 @@ btn.addEventListener('click',function () {
 //     }, animationTime / framesCount);
 //   });
 // });
-
-
-
 // function currentYPosition() {
 //     // Firefox, Chrome, Opera, Safari
 //     if (self.pageYOffset) return self.pageYOffset;
@@ -81,8 +66,6 @@ btn.addEventListener('click',function () {
 //     if (document.body.scrollTop) return document.body.scrollTop;
 //     return 0;
 // }
-
-
 // function elmYPosition(eID) {
 //     var elm = document.getElementById(eID);
 //     var y = elm.offsetTop;
@@ -92,8 +75,6 @@ btn.addEventListener('click',function () {
 //         y += node.offsetTop;
 //     } return y;
 // }
-
-
 // function smoothScroll(eID) {
 //     var startY = currentYPosition();
 //     var stopY = elmYPosition(eID);
@@ -117,15 +98,11 @@ btn.addEventListener('click',function () {
 //         leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
 //     }
 // }
-
   const anchors = document.querySelectorAll('a[href*="#"]')
-
   for (let anchor of anchors) {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
-
       const blockID = anchor.getAttribute('href');
-
       document.querySelector('' + blockID).scrollIntoView({
         behavior: 'smooth',
         block: 'start'
@@ -133,13 +110,8 @@ btn.addEventListener('click',function () {
     })
   }
 })();
-
-
-(()=>{
-
+(() => {
 })();
-
-
 //кнопка на вверх
 // $(function () {
 //   $(window).scroll(function () {
@@ -154,14 +126,12 @@ btn.addEventListener('click',function () {
 //     }, 800);
 //   });
 // });
-
-(function() {
+(function () {
   'use strict';
 
   function trackScroll() {
     let scrolled = window.pageYOffset;
     let coords = document.documentElement.clientHeight;
-
     if (scrolled > coords) {
       goTopBtn.classList.add('topButton__show');
     }
@@ -178,23 +148,19 @@ btn.addEventListener('click',function () {
   }
 
   const goTopBtn = document.querySelector('.topButton');
-
   window.addEventListener('scroll', trackScroll);
   goTopBtn.addEventListener('click', backToTop);
 })();
-
 //для мобильного меню
-(()=> {
+(() => {
   const burger = () => {
     const navBtn = document.getElementById('navMenu');
     navBtn.style.display === 'none' ? navBtn.style.display = 'block' : navBtn.style.display = 'none';
   };
   document.getElementById('btn').addEventListener('click', burger);
 })();
-
-
 //accordion2
-(()=>{
+(() => {
   const accordionButtons = document.querySelectorAll('.js-title');//1
   accordionButtons.forEach(btn => btn.addEventListener('click', () => {//2
     const isOpen = btn.nextElementSibling.classList.contains('accordion__copy--open');//7
@@ -217,6 +183,46 @@ btn.addEventListener('click',function () {
     item.style.maxHeight = 0;
     item.style.padding = "0 1.5rem 0 1.5rem";
   }
-
-
 })()
+
+// викторина
+(() => {
+  const test = document.getElementById('test');
+  const questions = [{
+    text: 'Сколько нот в музыке?',
+    right: 'семь'
+  },
+    {
+      text: 'Сколько нот в музыке?',
+      right: 'семь',
+    },
+    {
+      text: 'Сколько нот в музыке?',
+      right: 'семь'
+    }
+  ];
+  for (const question of questions) {
+    const div = document.createElement('div');
+    test.append('div');
+    const p = document.createElement('p');
+    p.textContent = question.text;
+    const input = document.createElement('input');
+    input.dataset.right = question.right;
+    div.append(p, input);
+  }
+  const button = document.getElementById('btn');
+  button.addEventListener('click', () => {
+    const inputs = document.querySelectorAll('input');
+    for(const inp of inputs) {
+      inp.classList.remove('.incorrect');
+      inp.classList.remove('.correct');
+
+      if(inp.value === inp.dataset.right) {
+        inp.classList.add('.correct')
+      } else {
+          inp.classList.add('.incorrect');
+        }
+    }
+  })
+})
+();
