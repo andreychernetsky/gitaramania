fc815c2b0f1b2380a3c5cd97b80be0fa9ad2cccf
andric <rodomir3119841985@gmail.com>
2020-11-16 19:14:30 +0300

    video

--- app/js/main.js
+++ app/js/main.js
@@ -352,3 +352,64 @@ button.disabled = true;
   }
 });
 //console.log(button);
+
+// slider
+{
+  class SlideShow {
+    constructor(startIndex, element) {
+      this.startIndex = startIndex;
+      this.currentIndex = this.startIndex;
+      this.element = element;
+      this.slides = this.element.querySelectorAll('.news__slide');
+
+      this.setActiveSlide();
+      this.next();
+      this.prev();
+    }
+
+    setActiveSlide() {
+      this.slides.forEach((item, index) => {
+        if (index === this.currentIndex
+        ) {
+          item.classList.add('active');
+        }
+        else {
+          item.classList.remove('active');
+        }
+      })
+    }
+
+    next() {
+      let nextBtn = this.element.querySelector('[data-way="next"]');
+      nextBtn.addEventListener('click', () => {
+        if (this.currentIndex === this.slides.length - 1
+        ) {
+          this.currentIndex = 0;
+        }
+        else {
+          this.currentIndex++;
+        }
+        this.setActiveSlide();
+      })
+    }
+
+    prev() {
+      let prevBtn = this.element.querySelector('[data-way="prev"]');
+      prevBtn.addEventListener('click', () => {
+        if (this.currentIndex === 0
+        ) {
+          this.currentIndex = this.slides.length - 1;
+        }
+        else {
+          this.currentIndex--;
+        }
+        this.setActiveSlide();
+      })
+    }
+  }
+
+  let slideShow = document.querySelectorAll('.news__slideshow');
+  slideShow.forEach(item => {
+    new SlideShow(0, item)
+  })
+}
